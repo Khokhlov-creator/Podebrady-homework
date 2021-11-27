3a6093c5bdba609c5e78f39493029c0098680925
Dmitrii Khokhlov <manus71111@gmail.com>
2021-11-27 13:52:45 +0100

    Du 6

---
+++ DU_6.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void mojefunkce (int *x, int y)
+{
+	*x=2*(*x);
+	x=y;
+	return;
+}
+
+int main()
+{
+	int i=2, *j;
+	j=(int*)malloc(sizeof(int));
+	mojefunkce(&i, &j);
+	printf("i = %d\n",i);
+	return 0;
+}
